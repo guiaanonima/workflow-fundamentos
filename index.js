bf2fde495b5bce7ec705968ceef5a23392eac74e
Keven S. Lopes <40838038+stephan-lopes@users.noreply.github.com>
2024-04-30 18:39:11 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const core = require('@actions/core');
 
 try {
   const meuInput = core.getInput('my-input');
-  console.log(`Hello ${meuInput}!`
-  core.setOutput("my-output", `Hello ${meuInput}!`)
+  console.log(`Hello ${meuInput}!`);
+  core.setOutput("my-output", `Hello ${meuInput}!`);
 } catch (error) {
-  core.setFailed(`Ação falhou com erro: ${error.message}`)
+  core.setFailed(`Ação falhou com erro: ${error.message}`);
 }
